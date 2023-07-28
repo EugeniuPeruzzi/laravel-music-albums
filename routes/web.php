272847f27497dbbe85c001d811078a89bbb80e86
Anieb22 <anielloverdino@gmail.com>
2023-07-28 13:00:03 +0200

    importato AlbumsController in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AlbumsController as AlbumsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/albums', [AlbumsController::class, 'index'])->name('albums.index');
