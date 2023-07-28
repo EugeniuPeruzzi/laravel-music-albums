b1b0c38f141f44fc3378ba924b2ae13e8e3c9e09
Anieb22 <anielloverdino@gmail.com>
2023-07-28 12:57:20 +0200

    modificato albumController con una funzione

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Album;
 class AlbumsController extends Controller
 {
-    //
+    public function index(){
+        $albums = Album::all();
+        return view('albums.index', compact('albums'));
+    }
 }
