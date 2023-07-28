07baaca0c4f148d8de8cb1d92e3092ae24c07e56
AntonelloStefania <antonelloste94@gmail.com>
2023-07-28 16:05:35 +0200

    creato seeder e inviato al db

---
+++ database/seeders/MusicAlbum.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Album;
+
+use Faker\Factory as Faker;
+use Faker\Provider\en_US\Person;
+
+class MusicAlbum extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        for($i=0; $i<=10; $i++){
+
+            $album = new Album();
+            $album->titolo = $faker->words(3, true);
+            $album->genere = $faker->randomElement(['Funk', 'Rock', 'Kpop', 'Neomelodica', 'Rap']);   
+            $album->autore = $faker->name();
+            $album->prezzo = $faker->randomFloat(2, 5, 99);
+
+            $album->save();
+            
+        }
+    }
+}
